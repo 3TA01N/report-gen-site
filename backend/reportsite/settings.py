d6e32401b9accf89793691cf0e84e6240982d3a2
3TA01N <kevinyue04@gmail.com>
2025-04-17 20:57:57 -0700

    tried hardcoding aws bucket name because of bug

--- backend/reportsite/settings.py
+++ backend/reportsite/settings.py
@@ -207,7 +207,7 @@ if USE_S3:
     # aws settings
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_STORAGE_BUCKET_NAME = "reportsite-djangobucket"
     AWS_REGION = os.environ.get('AWS_S3_REGION_NAME')
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'

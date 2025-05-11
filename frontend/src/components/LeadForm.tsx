d153b7bf037d3bd054365aa53196ec413ff034a1
3TA01N <kevinyue04@gmail.com>
2025-05-10 22:29:50 -0700

    added some instructions to lead form

--- frontend/src/components/LeadForm.tsx
+++ frontend/src/components/LeadForm.tsx
@@ -3,7 +3,12 @@ import {
     TextField,
     Button,
     Box,
+    IconButton,
+    Typography,
+    Tooltip,
   } from '@mui/material';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+  
 interface LeadFormProps {
     name: string;
     description: string;
@@ -14,6 +19,37 @@ interface LeadFormProps {
 function LeadForm({name, description, onNameChange, onDescriptionChange, onSubmit}: LeadFormProps) {
     return (
         <Box>
+            <Box position="relative" width="100%" display="flex" gap={1}>
+                <Typography variant="h5" color="primary" gutterBottom sx={{ paddingBottom: 0 }}>Create agent</Typography>
+                <Tooltip title={
+                    <Box>
+                    <Typography variant="subtitle2" gutterBottom>
+                        Leads keep meetings on track, choose agents, generate goals, and generates the final report. 
+                        Each lead keeps track of the reports it's been assigned to generate, and can (optionally) use past report information when generating new reports.
+                    </Typography>
+                  </Box>
+                }>
+                        <IconButton
+                        size="small"
+                        sx={{
+                            //position: 'absolute',
+                            top: '50%',
+                            right: 8,
+                            marginTop: '17.5px',
+                            transform: 'translateY(-50%)',
+                            backgroundColor: 'transparent',
+                            color: 'gray',
+                            '&:hover': {
+                            backgroundColor: 'transparent',
+                            color: 'black',
+                            },
+                        }}
+                        disableRipple
+                        >
+                        <HelpOutlineIcon />
+                        </IconButton>
+                    </Tooltip>
+            </Box>
             <Box component="form" onSubmit={onSubmit} display="flex" flexDirection="column" gap={3}>
                 <TextField
                     label="Name"
